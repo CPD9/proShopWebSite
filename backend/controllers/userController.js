e3b5863c04cda4e48344146731b55a2cc4ca2a8c
CPD9 <dikepeter9@gmail.com>
2021-03-17 18:20:21 +0100

    FaceLift

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,13 +10,14 @@ const authUser = asyncHandler(async (req, res) => {
 
     const user = await User.findOne({ email })
 
-    if (user && (await user.matchPassword(password))) {
+    if (user && (await user.matchPassword(password))
+    ) {
         res.json({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        isAdmin: user.isAdmin,
-        token: generateToken(user._id),
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
         })
     } else {
         res.status(401)
