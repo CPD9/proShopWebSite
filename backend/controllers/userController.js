9b36051cb69b4bf635a6da8c8a155ccd3c85aacf
CPD9 <dikepeter9@gmail.com>
2021-03-17 18:11:32 +0100

    User Registeration and password hashing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,12 +6,45 @@ import User from '../models/userModel.js'
 // @route   POST /api/users/login
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
-const { email, password } = req.body
+    const { email, password } = req.body
 
-const user = await User.findOne({ email })
+    const user = await User.findOne({ email })
 
-if (user && (await user.matchPassword(password))) {
-    res.json({
+    if (user && (await user.matchPassword(password))) {
+        res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user._id),
+        })
+    } else {
+        res.status(401)
+        throw new Error('Invalid email or password')
+    }
+})
+
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+const { name, email, password } = req.body
+
+const userExists = await User.findOne({ email })
+
+if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+}
+
+const user = await User.create({
+    name,
+    email,
+    password,
+})
+
+if (user) {
+    res.status(201).json({
     _id: user._id,
     name: user.name,
     email: user.email,
@@ -19,10 +52,11 @@ if (user && (await user.matchPassword(password))) {
     token: generateToken(user._id),
     })
 } else {
-    res.status(401)
-    throw new Error('Invalid email or password')
+    res.status(400)
+    throw new Error('Invalid user data')
 }
 })
+ 
 
 // @desc    Get user profile
 // @route   GET /api/users/profile
@@ -43,4 +77,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
     }
 })
 
-export { authUser, getUserProfile }
+export { authUser, registerUser, getUserProfile }
