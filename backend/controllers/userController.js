a7e7b2e110061eda517ea773498110ccd9ca8d86
CPD9 <dikepeter9@gmail.com>
2021-03-18 21:01:08 +0100

    User login reducer & action

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,8 +10,8 @@ const authUser = asyncHandler(async (req, res) => {
 
     const user = await User.findOne({ email })
 
-    if (user && (await user.matchPassword(password))
-    ) {
+    if (user && (await user.matchPassword(password))) {
+    
         res.json({
             _id: user._id,
             name: user.name,
@@ -29,33 +29,33 @@ const authUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-const { name, email, password } = req.body
+    const { name, email, password } = req.body
 
-const userExists = await User.findOne({ email })
+    const userExists = await User.findOne({ email })
 
-if (userExists) {
-    res.status(400)
-    throw new Error('User already exists')
-}
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
 
-const user = await User.create({
-    name,
-    email,
-    password,
-})
-
-if (user) {
-    res.status(201).json({
-    _id: user._id,
-    name: user.name,
-    email: user.email,
-    isAdmin: user.isAdmin,
-    token: generateToken(user._id),
+    const user = await User.create({
+        name,
+        email,
+        password,
     })
-} else {
-    res.status(400)
-    throw new Error('Invalid user data')
-}
+
+    if (user) {
+        res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user._id),
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
 })
  
 
--- backend/data/users.js
+++ backend/data/users.js
@@ -1,22 +1,46 @@
-import bcyrpt from 'bcryptjs'
+import bcrypt from 'bcryptjs'
 
 const users = [
     {
         name: 'Admin User',
         email: 'admin@example.com',
-        password: bcyrpt.hashSync('123456', 10),
+        password: bcrypt.hashSync('123456', 10),
         isAdmin: true,
     },
     {
         name: 'Salam Ahmed',
         email: 'salam@gmail.com',
-        password: bcyrpt.hashSync('123456', 10),
+        password: bcrypt.hashSync('123456', 10),
     },
     {
         name: 'Chuks King',
         email: 'chuks@example.com',
-        password: bcyrpt.hashSync('123456', 10),
+        password: bcrypt.hashSync('123456', 10),
     },
 ]
 
 export default users
+
+
+// import bcrypt from 'bcryptjs'
+
+// const users = [
+//   {
+//     name: 'Admin User',
+//     email: 'admin@example.com',
+//     password: bcrypt.hashSync('123456', 10),
+//     isAdmin: true,
+//   },
+//   {
+//     name: 'John Doe',
+//     email: 'john@example.com',
+//     password: bcrypt.hashSync('123456', 10),
+//   },
+//   {
+//     name: 'Jane Doe',
+//     email: 'jane@example.com',
+//     password: bcrypt.hashSync('123456', 10),
+//   },
+// ]
+
+// export default users
