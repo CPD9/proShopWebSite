a9abfb787bb985501538c5e24d7fc3e9fc5dd979
CPD9 <dikepeter9@gmail.com>
2021-03-19 17:43:23 +0100

    Admin user list

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -3,6 +3,9 @@ import {
     USER_DETAILS_REQUEST,
     USER_DETAILS_RESET,
     USER_DETAILS_SUCCESS,
+    USER_LIST_REQUEST,
+    USER_LIST_SUCCESS,
+    USER_LIST_FAIL,
     USER_LOGIN_FAIL,
     USER_LOGIN_REQUEST,
     USER_LOGIN_SUCCESS,
@@ -29,7 +32,6 @@ export const userLoginReducer = (state = {}, action) => {
         default:
             return state
     }
-
 }
 
 export const userRegisterReducer = (state = {}, action) => {
@@ -73,4 +75,17 @@ export const userUpdateProfileReducer = (state = {}, action) => {
       default:
         return state
     }
+}
+
+export const userListReducer = (state = { users: [] }, action) => {
+  switch (action.type) {
+    case USER_LIST_REQUEST:
+      return { loading: true }
+    case USER_LIST_SUCCESS:
+      return { loading: false, users: action.payload }
+    case USER_LIST_FAIL:
+      return { loading: false, error: action.payload }
+    default:
+      return state
+  }
 }
